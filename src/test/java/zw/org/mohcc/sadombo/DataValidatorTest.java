58d847dd9dfe79fe16b74b6d8530ae7a6fa93900
Charles Chigoriwa <cchigoriwa@gmail.com>
2017-12-16 07:29:50 +0200

    Include Testes

---
+++ src/test/java/zw/org/mohcc/sadombo/DataValidatorTest.java
@@ -0,0 +1,35 @@
+package zw.org.mohcc.sadombo;
+
+import java.io.IOException;
+import java.io.InputStream;
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author Charles Chigoriwa
+ */
+public class DataValidatorTest {
+
+    @Ignore
+    @Test
+    public void test() throws SAXException, IOException {
+
+        InputStream xsdInputStream = MediatorMain.class.getClassLoader().getResourceAsStream("zw/org/mohcc/sadombo/data/ATB_005.xsd");
+        InputStream xmlInputStream = MediatorMain.class.getClassLoader().getResourceAsStream("zw/org/mohcc/sadombo/data/sample_data.xml");
+
+        SchemaFactory factory
+                = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        Schema schema = factory.newSchema(new StreamSource(xsdInputStream));
+        Validator validator = schema.newValidator();
+        validator.validate(new StreamSource(xmlInputStream));
+
+    }
+
+}
