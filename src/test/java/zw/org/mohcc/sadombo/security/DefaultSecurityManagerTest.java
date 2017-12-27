9bb3af6908cdf55e7139aa577f725e36c73f5350
Charles Chigoriwa <cchigoriwa@gmail.com>
2017-12-27 13:18:56 +0200

    Added mocking in DhisOrchestratorTest(II)

--- src/test/java/zw/org/mohcc/sadombo/security/DefaultSecurityManagerTest.java
+++ src/test/java/zw/org/mohcc/sadombo/security/DefaultSecurityManagerTest.java
@@ -10,12 +10,14 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import org.openhim.mediator.engine.MediatorConfig;
 import org.openhim.mediator.engine.messages.MediatorHTTPRequest;
 import zw.org.mohcc.sadombo.Channels;
-import zw.org.mohcc.sadombo.DefaultSadomboBeanFactory;
-import zw.org.mohcc.sadombo.EnhancedChannels;
+import zw.org.mohcc.sadombo.SadomboBeanFactory;
 import zw.org.mohcc.sadombo.utils.ConfigUtility;
+import zw.org.mohcc.sadombo.utils.Credentials;
 import zw.org.mohcc.sadombo.utils.GeneralUtility;
 
 /**
@@ -25,66 +27,56 @@ import zw.org.mohcc.sadombo.utils.GeneralUtility;
 public class DefaultSecurityManagerTest {
 
     private MediatorConfig testConfig;
-    private DefaultSecurityManager mediatorSecurityService;
+    private DefaultSecurityManager defaultSecurityManager;
 
     @Before
     public void setUp() throws Exception {
         testConfig = new MediatorConfig("sadombo", "localhost", 3000);
-        //TODO: replace with mocks pleases
-        testConfig.getDynamicConfig().put("sadomboBeanFactory", DefaultSadomboBeanFactory.getInstance(null, (String[]) null));
-        mediatorSecurityService = new DefaultSecurityManager();
+        SadomboBeanFactory sadomboBeanFactory = mock(SadomboBeanFactory.class);
+        testConfig.getDynamicConfig().put("sadomboBeanFactory", sadomboBeanFactory);
+        Channels channels = mock(Channels.class);
+        when(sadomboBeanFactory.getChannels()).thenReturn(channels);
+        defaultSecurityManager = new DefaultSecurityManager();
     }
 
     @After
     public void tearDown() throws Exception {
         testConfig = null;
-        mediatorSecurityService = null;
+        defaultSecurityManager = null;
     }
 
     @Test
     public void test() {
         MediatorHTTPRequest request = getMediatorHTTPRequest(null, null);
         Channels channels = ConfigUtility.getChannels(testConfig);
-        //TODO: Replace with mocking here please
-        ((EnhancedChannels) channels).setSadomboAuthenticationEnabled(false);
-        //Replace the above code with mocking please
-        assertTrue(mediatorSecurityService.isUserAllowed(request, testConfig));
+        when(channels.isSadomboAuthenticationEnabled()).thenReturn(false);
+        assertTrue(defaultSecurityManager.isUserAllowed(request, testConfig));
     }
 
     @Test
     public void testWithAuthEnabledButNoAuthorization() {
         Channels channels = ConfigUtility.getChannels(testConfig);
-        //TODO: Replace with mocking here please :: Mock both SadomboBeanFactory and Channels
-        ((EnhancedChannels) channels).setSadomboAuthenticationEnabled(true);
-        //Replace the above code with mocking please
-
+        when(channels.isSadomboAuthenticationEnabled()).thenReturn(true);
         MediatorHTTPRequest request = getMediatorHTTPRequest(null, null);
-        assertFalse(mediatorSecurityService.isUserAllowed(request, testConfig));
+        assertFalse(defaultSecurityManager.isUserAllowed(request, testConfig));
     }
 
     @Test
     public void testWithCorrectCredentials() {
-
         Channels channels = ConfigUtility.getChannels(testConfig);
-        //TODO: Replace with mocking here please
-        ((EnhancedChannels) channels).setSadomboAuthenticationEnabled(true);
-        ((EnhancedChannels) channels).getSadomboCredentials().setUsername("user");
-        ((EnhancedChannels) channels).getSadomboCredentials().setPassword("password");
-        //Replace the above code with mocking please
+        when(channels.isSadomboAuthenticationEnabled()).thenReturn(true);
+        when(channels.getSadomboCredentials()).thenReturn(new Credentials("user", "password"));
         MediatorHTTPRequest request = getMediatorHTTPRequest("user", "password");
-        assertTrue(mediatorSecurityService.isUserAllowed(request, testConfig));
+        assertTrue(defaultSecurityManager.isUserAllowed(request, testConfig));
     }
 
     @Test
     public void testWithWrongCredentials() {
         Channels channels = ConfigUtility.getChannels(testConfig);
-        //TODO: Replace with mocking here please
-        ((EnhancedChannels) channels).setSadomboAuthenticationEnabled(true);
-        ((EnhancedChannels) channels).getSadomboCredentials().setUsername("user");
-        ((EnhancedChannels) channels).getSadomboCredentials().setPassword("password");
-        //Replace the above code with mocking please
+        when(channels.isSadomboAuthenticationEnabled()).thenReturn(true);
+        when(channels.getSadomboCredentials()).thenReturn(new Credentials("user", "password"));
         MediatorHTTPRequest request = getMediatorHTTPRequest("user1", "password");
-        assertFalse(mediatorSecurityService.isUserAllowed(request, testConfig));
+        assertFalse(defaultSecurityManager.isUserAllowed(request, testConfig));
     }
 
     private MediatorHTTPRequest getMediatorHTTPRequest(String username, String password) {
